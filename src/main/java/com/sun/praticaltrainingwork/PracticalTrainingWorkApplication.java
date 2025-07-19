fd67799a9ebe7f096a3080870ffb81ff9e3ede46
Lizihao <140171952+Areo223@users.noreply.github.com>
2025-07-19 16:48:03 +0800

    诊疗项目信息维护

--- src/main/java/com/sun/praticaltrainingwork/PracticalTrainingWorkApplication.java
+++ src/main/java/com/sun/praticaltrainingwork/PracticalTrainingWorkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PraticalTrainingWorkApplication {
+public class PracticalTrainingWorkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PraticalTrainingWorkApplication.class, args);
+        SpringApplication.run(PracticalTrainingWorkApplication.class, args);
     }
 
 }
