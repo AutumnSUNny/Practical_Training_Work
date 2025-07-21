d31a1387f88bcef7306a0035b72ddd214ca4a5c1
Areo <140171952+Areo223@users.noreply.github.com>
2025-07-21 10:51:03 +0800

    医疗机构模块
    * 诊疗项目信息维护
    
    * 医疗机构模块

---
+++ src/main/java/com/sun/praticaltrainingwork/mapper/MedicalInsititutionMapper.java
@@ -0,0 +1,9 @@
+package com.sun.praticaltrainingwork.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sun.praticaltrainingwork.domain.DO.TMedicalInsititution;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MedicalInsititutionMapper extends BaseMapper<TMedicalInsititution> {
+}
