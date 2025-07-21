b9b7f6217be7666aebee5d5ebe1b5049e9ab0d2d
Lizihao <140171952+Areo223@users.noreply.github.com>
2025-07-21 08:29:58 +0800

    诊疗项目信息维护

--- src/main/java/com/sun/praticaltrainingwork/domain/DTO/DiagnosisIdRequest.java
+++ src/main/java/com/sun/praticaltrainingwork/domain/DTO/DiagnosisIdRequest.java
@@ -1,8 +1,10 @@
 package com.sun.praticaltrainingwork.domain.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class DiagnosisIdRequest {
+    @Schema(description = "项目ID")
     private Integer id;;
 }
