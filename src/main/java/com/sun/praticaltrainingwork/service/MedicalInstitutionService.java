b30877b0adb4f53c043a51f56e94702978294c8d
Lizihao <140171952+Areo223@users.noreply.github.com>
2025-07-22 08:27:06 +0800

    医疗待遇审批

--- src/main/java/com/sun/praticaltrainingwork/service/MedicalInstitutionService.java
+++ src/main/java/com/sun/praticaltrainingwork/service/MedicalInstitutionService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-@Service
+
 public interface MedicalInstitutionService {
     Result<Void> addInstitutionInformation(TMedicalInsititution tMedicalInsititution);
 
